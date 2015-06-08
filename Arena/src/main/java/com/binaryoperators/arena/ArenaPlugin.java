f549964a231f943d7cd19ddbad8e8a1f02ea90c9
Matthew Fransen <2016mfransen@gmail.com>
2015-06-08 12:02:40 -0500

    Renamed Arena to ArenaPlugin.

--- Arena/src/main/java/com/binaryoperators/arena/ArenaPlugin.java
+++ Arena/src/main/java/com/binaryoperators/arena/ArenaPlugin.java
@@ -3,6 +3,6 @@ package com.binaryoperators.arena;
 import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id="Arena",name="Arena",version="0.0.0.1")
-public class Arena {
+public class ArenaPlugin {
 	
 }
